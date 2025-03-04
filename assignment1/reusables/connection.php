ed536832161a4ea296c9aa7bbe3380e10fce53fb
Janineint <janineintra@gmail.com>
2025-03-04 18:58:39 -0500

    update database connection

--- assignment1/reusables/connection.php
+++ assignment1/reusables/connection.php
@@ -1,9 +1,9 @@
 <?php
     $connect = mysqli_connect(
-          'localhost', //URL
-          'root', //Username
-          'root', //Your password here, either root or empty
-          '5225_test' // your database name, check your PHP myAdmin
+          'sql303.infinityfree.com', //URL
+          'if0_38447676', //Username
+          'xJ8mMZkXQe3ZUni', //Your password here, either root or empty
+          'if0_38447676_5225_test' // your database name, check your PHP myAdmin
         );
         if(!$connect){
           echo 'Error Code: ' . mysqli_connect_errno();
